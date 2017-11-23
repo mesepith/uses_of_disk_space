1e46ee8ca1623ac680cb2f6aad20218971a8c942
zahiralam <zahir.alam@switchme.in>
2017-11-23 16:33:45 +0530

    remove blank value from exple array

--- check.php
+++ check.php
@@ -1,32 +1,50 @@
 <?php
 
-
 	$partition_name = '/dev/sda2';
 	
 	$disk_space = exec("df -hl " . $partition_name);
 	
 	$explode_disk_space = explode(" ", $disk_space);
-	
+        
+        $disk_arr = array();
+        
+        $count=0;
+        
+        for( $i=0; $i<count($explode_disk_space); $i++){
+            
+            if( ( !empty($explode_disk_space[$i]) ) ){
+                
+                $count++;
+                
+                $disk_arr[$count] = $explode_disk_space[$i];
+                
+            }
+            
+        }
+        
 	$disk_space_arr = array();
 	
-	$total_size = $explode_disk_space[7];
+	$total_size = $disk_arr[2];
 	
 	$disk_space_arr['total_size'] = $total_size;
 	
-	$used = $explode_disk_space[9];
+	$used = $disk_arr[3];
 	
 	$disk_space_arr['used'] = $used;
 	
-	$available = $explode_disk_space[11];
+	$available = $disk_arr[4];
 	
 	$disk_space_arr['available'] = $available;
 	
-	$perchantage_of_uses = $explode_disk_space[13];
+	$perchantage_of_uses = $disk_arr[5];
 	
 	$disk_space_arr['total_Uses_in_perchantage'] = $perchantage_of_uses;
 	
 	echo '<pre>';
 	print_r($disk_space_arr);
+        
+	return $disk_space_arr;
 
 
 ?>
+
